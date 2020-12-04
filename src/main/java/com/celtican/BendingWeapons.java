21f70edf7e251c76302660bc1d4834ab3eba6008
Celtican <Celtican5@mail.com>
2020-12-04 00:13:51 -0500

    Removed NBT API That's for something similar, but different. Looks like I don't need to parse NBT, spigot allows me to fetch the attributes of items. Oops!

--- src/main/java/com/celtican/BendingWeapons.java
+++ src/main/java/com/celtican/BendingWeapons.java
@@ -1,7 +1,6 @@
 package com.celtican;
 
 import com.projectkorra.projectkorra.ability.CoreAbility;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BendingWeapons extends JavaPlugin {
@@ -9,12 +8,9 @@ public class BendingWeapons extends JavaPlugin {
     public static String AUTHOR = "Celtican";
     public static String VERSION = "0.1";
 
-    public static boolean isNBTEnabled;
-
     @Override public void onEnable() {
         getServer().getPluginManager().registerEvents(new AbilityListener(), this);
         CoreAbility.registerPluginAbilities(this, "com.celtican.abilities");
-        isNBTEnabled = getServer().getPluginManager().getPlugin("NBTAPI") != null;
     }
 
     @Override public void onDisable() {
