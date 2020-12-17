7ab6f0fde9f4717c383383be822f78e8b92b1703
Celtican <Celtican5@mail.com>
2020-12-16 21:32:17 -0500

    Changed stamina bar appearance

--- src/main/java/com/celtican/stamina/StaminaPlayer.java
+++ src/main/java/com/celtican/stamina/StaminaPlayer.java
@@ -1,5 +1,6 @@
 package com.celtican.stamina;
 
+import com.projectkorra.projectkorra.BendingPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -8,10 +9,12 @@ import java.util.UUID;
 public class StaminaPlayer extends StaminaEntity {
 
     public final Player player;
+    public final BendingPlayer bPlayer;
 
     protected StaminaPlayer(UUID uuid) {
         super(uuid);
         player = Bukkit.getPlayer(uuid);
+        bPlayer = BendingPlayer.getBendingPlayer(player);
     }
 
     @Override public boolean isPlayer() {
@@ -23,14 +26,26 @@ public class StaminaPlayer extends StaminaEntity {
         StringBuilder message = new StringBuilder("§2[ ");
 
         int numPipes = 5;
-        int numCharacters = (int) (maxStamina*numPipes + maxStamina-1);
-        int numLit = (int) (numCharacters * stamina / maxStamina);
 
-        for (int i = 0; i < numCharacters; i++) {
-            message.append(i < numLit ? "§a" : "§8").append((i+1) % (numPipes+1) == 0 ? ':' : '|');
+        for (int curStamina = 0; curStamina < maxStamina; curStamina++) {
+            if (stamina >= curStamina + 1) {
+                message.append("§a||||| ");
+                continue;
+            } else if (stamina <= curStamina) {
+                message.append("§8||||| ");
+                continue;
+            }
+
+            int numPipesColored = (int) (numPipes * (stamina%1));
+            message.append("§a");
+            int pipe;
+            for (pipe = 0; pipe < numPipesColored; pipe++) message.append("|");
+            message.append("§8");
+            for (; pipe < numPipes; pipe++) message.append("|");
+            message.append(" ");
         }
 
-        message.append(" §2]");
+        message.append("§2]");
 
         player.sendActionBar(message.toString());
     }
